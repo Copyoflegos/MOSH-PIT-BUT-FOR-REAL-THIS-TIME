219a83a60c5b7c56055154b3ec2c81292c31f402
geirgie25 <43558674+geirgie25@users.noreply.github.com>
2018-11-02 22:22:48 -0500

    stuff

---
+++ MoshPit/Assets/ScriptsR/Player/Projectiles/DrumProjectileInstScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumProjectileInstScript : MonoBehaviour {
+
+    [SerializeField]
+    float deathTimer;
+
+    private void Awake()
+    {
+        transform.position += transform.right * 1f;
+        StartCoroutine("killSelf");
+    }
+
+    IEnumerator killSelf()
+    {
+        yield return new WaitForSeconds(deathTimer);
+        Destroy(gameObject);
+    }
+
+}
---
+++ MoshPit/Assets/ScriptsR/Player/Projectiles/PianoProjectileScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoProjectileScript : MonoBehaviour {
+
+    [SerializeField]
+    float deathTimer;
+
+    private void Awake()
+    {
+        StartCoroutine("killSelf");
+    }
+
+    IEnumerator killSelf()
+    {
+        yield return new WaitForSeconds(deathTimer);
+        Destroy(gameObject);
+    }
+}
