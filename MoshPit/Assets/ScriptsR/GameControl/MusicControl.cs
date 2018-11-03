c99a5144d4efa93d155b37b60f5cc37f260957eb
Copyoflegos <Copyoflegos@gmail.com>
2018-11-02 19:38:23 -0500

    IT"S THE MOSH PIT
    it's the mostest" 0- fgay

---
+++ MoshPit/Assets/ScriptsR/GameControl/MusicControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour
+{
+    public readonly float CurrentTempo;
+
+    [SerializeField]
+    Song[] SongPlaylist;
+    [SerializeField]
+    AudioSource[] Audios;
+    [SerializeField]
+    float Volume;
+
+    int CurrentSongID = 0;
+    Song CurrentSong;
+
+    private void Awake()
+    {
+        CurrentSong = SongPlaylist[0];
+        print(SongPlaylist.Length);
+        Audios[1].volume = 0;
+        Audios[2].volume = 0;
+        Audios[0].volume = Volume;
+    }
+
+    public void ChangeTrack()
+    {
+        if(CurrentSongID + 1< SongPlaylist.Length)
+        {
+            
+            CurrentSongID += 1;
+            print(CurrentSongID);
+        }
+        else
+        {
+            CurrentSongID = 0;
+            print("what");
+        }
+        
+        CurrentSong = SongPlaylist[CurrentSongID];
+        Audios[CurrentSongID].volume = Volume;
+        foreach(AudioSource AS in Audios)
+        {
+            if(AS != Audios[CurrentSongID])
+            {
+                AS.volume = 0;
+            }
+        }
+    }
+    
+    public float GetTempo()
+    {
+        return CurrentSong.Tempo;
+    }
+    public float GetSwitch()
+    {
+        return CurrentSong.SongSwitchWait;
+    }
+    public GameObject GetBullet()
+    {
+        return CurrentSong.Bullet;
+    }
+}
---
+++ MoshPit/Assets/ScriptsR/Player/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+
+
+    bool Right;
+    bool Left;
+    bool Up;
+    bool Down;
+    
+   
+    //temp velocity to add to player 
+    Vector3 velocity;
+
+    //Acceleration Speed
+    public float AccSpeed;
+    
+
+    Rigidbody2D Rb;
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody2D>();   
+    }
+
+
+    private void Update()
+    {
+        //Set bools equal to input (GetButton is for Input from project settings)
+        Right = Input.GetButton("Right");
+        Left = Input.GetButton("Left");
+        Up = Input.GetButton("Up");
+        Down = Input.GetButton("Down");
+       
+
+        //Series of ifs for detecting input and adding to velocity
+        if (Right )
+        {
+            velocity.x = AccSpeed;
+        }
+        if (Left )
+        {
+            velocity.x = -AccSpeed;
+        }
+        if (Up )
+        {
+            velocity.y = AccSpeed;
+        }
+        if (Down )
+        {
+            velocity.y = -AccSpeed;
+        }
+
+        if(Left && Right)
+        {
+            velocity.x = 0;
+        }
+        if(Up && Down)
+        {
+            velocity.y = 0;
+        }
+
+        
+        if(!Right && !Left)
+        {
+            velocity.x = 0;
+        }
+        if(!Up && !Down)
+        {
+            velocity.y = 0;
+        }
+
+        
+        
+    }
+    private void FixedUpdate()
+    {
+        Rb.AddForce(velocity);
+        
+    }
+}
